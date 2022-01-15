c39aa12a5758ca5a466553e3ea23443dc7525dcc
pctmustang <85476908+leechoiswim1@users.noreply.github.com>
2022-01-15 21:40:52 +0900

    [add] 오델로 기초 작업
    - 기본 div 셋팅
    - 클릭시 turn변경 및 disc 추가

--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+// STYLE
+import styled from "styled-components";
 function App() {
+  const [playBoxs, setPlayBoxs] = useState([
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 2, 0, 0, 0],
+    [0, 0, 0, 2, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+
+  const [turn, setTurn] = useState("W");
+
+  const ClickDisc = (disc, mainIdx, innerIdx) => {
+    if (turn === "W") {
+      setPlayBoxs(
+        playBoxs.map((items, idx) => {
+          return idx === mainIdx
+            ? items.map((item, idx) => {
+                return idx === innerIdx ? 2 : item;
+              })
+            : items;
+        })
+      );
+      setTurn("B");
+    } else if (turn === "B") {
+      setPlayBoxs(
+        playBoxs.map((items, idx) => {
+          return idx === mainIdx
+            ? items.map((item, idx) => {
+                return idx === innerIdx ? 1 : item;
+              })
+            : items;
+        })
+      );
+      setTurn("W");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MainBox>
+      <InnerBox>
+        {playBoxs.map((playBox, mainIdx) => {
+          return (
+            <PlayBoxBar key={mainIdx}>
+              {playBox.map((disc, innerIdx) => {
+                return (
+                  <PlayBox
+                    key={innerIdx}
+                    onClick={() => {
+                      ClickDisc(disc, mainIdx, innerIdx);
+                    }}
+                  >
+                    <PlayDisc disc={disc}></PlayDisc>
+                  </PlayBox>
+                );
+              })}
+            </PlayBoxBar>
+          );
+        })}
+      </InnerBox>
+    </MainBox>
   );
 }
+
+const MainBox = styled.div`
+  background: brown;
+  width: 850px;
+  height: 850px;
+  margin: auto;
+  display: flex;
+`;
+const InnerBox = styled.div`
+  background: green;
+  width: 800px;
+  height: 800px;
+  margin: auto;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+`;
+
+const PlayBoxBar = styled.div`
+  width: 800px;
+  height: 100px;
+  margin: auto;
+  display: flex;
+`;
+const PlayBox = styled.div`
+  width: 100px;
+  height: 100px;
+  margin: auto;
+  box-sizing: border-box;
+  border: 1px solid black;
+  display: flex;
+`;
+
+const PlayDisc = styled.div`
+  background: ${(props) =>
+    props.disc === 0 ? "none" : props.disc === 1 ? "black" : "white"};
+  cursor: ${(props) => (props.disc === 0 ? "pointer" : "")};
+  width: 80px;
+  height: 80px;
+  border-radius: 50%;
+  margin: auto;
+`;
 
 export default App;
